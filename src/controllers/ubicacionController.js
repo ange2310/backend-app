b0746a4a6cb905ce5fb5188890541d126476fb4a
Angelica Marcillo <angelicamariaalba049@gmail.com>
2025-05-31 04:43:42 -0500

    Agregar controlador de ubicación (geolocalización y mapas)

--- src/controllers/ubicacionController.js
+++ src/controllers/ubicacionController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
+// Controlador para manejar las ubicaciones de pedidos
 exports.obtenerUbicacionPedido = async (req, res) => {
   try {
     const { pedidoId } = req.params;
