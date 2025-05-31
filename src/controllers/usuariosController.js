a556e924fde05bbd1e3b976e2189b708e7714fbf
Angelica Marcillo <angelicamariaalba049@gmail.com>
2025-05-31 04:43:57 -0500

    Agregar controlador de usuarios (perfil, CRUD usuarios)

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require('bcrypt');
+// Controlador para manejar las operaciones relacionadas con los usuarios
 exports.getUserProfile = async (req, res) => {
   try {
     const userId = req.user.id;
